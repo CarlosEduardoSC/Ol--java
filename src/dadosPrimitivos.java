1aa6d6a277113c8b7d90d5e13e4dad792dd88dc7
DUDU <carloseduardosouzacaetano@gmail.com>
2024-08-01 18:56:02 -0300

    Dados Primitivos

---
+++ src/dadosPrimitivos.java
@@ -0,0 +1,24 @@
+public class dadosPrimitivos {
+    //Letras e simbolos (Textos)
+    public static void main(String[]args){
+        //Variavel do tipo String
+        String txt = "Carlos Eduardo";
+            System.out.println(txt);
+
+        //Constante
+        final String txt2 = "Souza Caetano";
+            System.out.println(txt2);
+
+    //Numeros
+        //inteiros
+            int numeroInteiro = 12;
+                System.out.println(numeroInteiro);
+
+        //Decimais
+            double numeroDouble = 7.7;
+                System.out.println(numeroDouble);
+            }
+    //Logicos
+
+
+}
