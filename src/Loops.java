680df1314b3b86fd31f27b5d0af054eb7f19df71
DUDU <carloseduardosouzacaetano@gmail.com>
2024-08-06 19:17:43 -0300

    Loop

---
+++ src/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String []args){
+        int[] ArreySimples = {1,2,3,4,5};
+
+        for (int i =0; i < ArreySimples.length; i ++){
+            System.out.println(ArreySimples[i]);
+        }
+
+        System.out.println(ArreySimples.length);
+    }
+}
