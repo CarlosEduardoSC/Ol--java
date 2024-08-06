5405844df539f654e57c13f76d81ba8b2b4d50bc
DUDU <carloseduardosouzacaetano@gmail.com>
2024-08-06 19:07:23 -0300

    If else e Lista de dados

---
+++ src/IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse {
+    public static void main(String[]args){
+        int numero1 = 10;
+
+        int numero2 = 20;
+
+        if (numero1 > numero2){
+            System.out.println("Numero 1 é maior que o numero 2");
+        } else if (numero1 == numero2){
+            System.out.println("numero 1 é igual ao 2");
+        } else {
+            System.out.println("Numero 2 que é o maior");
+        }
+    }
+}
---
+++ src/ListaDeDados.java
@@ -0,0 +1,17 @@
+public class ListaDeDados {
+    public static void main (String[]args){
+      int[]Arrey = {1,3,76,345};
+      int []Arrey2 = {2,4,78,456};
+        int Arrey3 [] = {8,0,71,855};
+
+        System.out.println(Arrey[1]);
+
+        int[] Arrey4 = new int[4];
+        Arrey4[0] = 2580;
+        Arrey4[1] = 2581;
+        Arrey4[2] = 2582;
+        Arrey4[1] = 2585;
+
+        System.out.println(Arrey4[2]);
+    }
+}
